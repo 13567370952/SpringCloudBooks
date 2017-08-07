31ce2978dd773a9d5f339b30ebb26b052fbfd83e
wujunshen <frank_wjs@hotmail.com>
2017-08-07 22:59:00 +0800

    service-discovery子项目重构

--- spring-cloud-service-discovery/src/main/java/com/wujunshen/ServiceDiscoveryApplication.java
+++ spring-cloud-service-discovery/src/main/java/com/wujunshen/ServiceDiscoveryApplication.java
@@ -1,5 +1,7 @@
 package com.wujunshen;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,7 +9,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceDiscoveryApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceDiscoveryApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("start execute ServiceDiscoveryApplication....\n");
         SpringApplication.run(ServiceDiscoveryApplication.class, args);
+        LOGGER.info("end execute ServiceDiscoveryApplication....\n");
     }
 }
