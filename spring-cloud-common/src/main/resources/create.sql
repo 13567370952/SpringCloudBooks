4d235f86d697153500bc5db195b0b8dd630cd95c
wujunshen <frank_wjs@hotmail.com>
2017-09-05 16:44:44 +0800

    sql脚本数据库表定义修改 sql脚本初始化数据增加

--- spring-cloud-common/src/main/resources/create.sql
+++ spring-cloud-common/src/main/resources/create.sql
@@ -1,22 +1,25 @@
 CREATE TABLE
-    user
-    (
-        id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(255),
-        password VARCHAR(255),
-        salt VARCHAR(255),
-        role VARCHAR(255),
-        PRIMARY KEY (id)
-    )
-    ENGINE=MyISAM DEFAULT CHARSET=utf-8;
+  user
+(
+  userId   INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(255),
+  password VARCHAR(255),
+  salt     VARCHAR(255),
+  role     VARCHAR(255),
+  PRIMARY KEY (userId)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE
-    simple_book
-    (
-        bookName VARCHAR(255),
-        publisher VARCHAR(255) COLLATE utf8_general_ci,
-        bookId INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY (bookId)
-    )
-    ENGINE=MyISAM DEFAULT CHARSET=utf-8;
+  simple_book
+(
+  bookName  VARCHAR(255),
+  publisher VARCHAR(255)
+            COLLATE utf8_general_ci,
+  bookId    INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (bookId)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
